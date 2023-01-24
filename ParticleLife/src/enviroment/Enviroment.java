390784f7d208021fe6bd6f94e4ac9b578b2345b2
robobo789 <jozawalek@gmail.com>
2023-01-24 12:18:54 +0100

    import organization

--- ParticleLife/src/enviroment/Enviroment.java
+++ ParticleLife/src/enviroment/Enviroment.java
@@ -10,7 +10,6 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import camera.Camera;
 import hashgrid.SpatialHashGrid;
